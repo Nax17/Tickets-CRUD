8fa76c5a564cd3edf92822b6fd4ffc148992a478
Nax17 <33287649+Nax17@users.noreply.github.com>
2019-06-03 00:43:44 -0400

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { HomeComponent } from './home/home.component';
+import { ExternoComponent } from './externo/externo.component';
+import { TrabajadorComponent } from './trabajador/trabajador.component';
+import { PiscinaComponent } from './piscina/piscina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'estudiante', component: EstudianteComponent },
+  { path: 'trabajador', component: TrabajadorComponent },
+  { path: 'externo', component: ExternoComponent },
+  { path: 'piscina', component: PiscinaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { TrabajadorComponent } from './trabajador/trabajador.component';
+import { ExternoComponent } from './externo/externo.component';
+import { PiscinaComponent } from './piscina/piscina.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    EstudianteComponent,
+    TrabajadorComponent,
+    ExternoComponent,
+    PiscinaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
